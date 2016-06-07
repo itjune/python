8097003262b6b3fb09e0cf203e54d88039aed59f
jane <spirit1334@yandex.ru>
2016-06-07 18:06:12 +0500

    finish script

--- network/port_scan/port_scan.py
+++ network/port_scan/port_scan.py
@@ -1,15 +1,45 @@
 import socket
+import sys
 
-def scan_ports(ip, port_list, timeout=10):
+def scan_ports(ip, port_list, timeout):
     socket.setdefaulttimeout(timeout)
     for port in port_list:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((ip, port))
-        if sock:
+        if sock.connect_ex((ip, port)) == 0:
             print('Port %d is open' % (port))
             sock.close()
         else:
             print('Port %d is close' % (port))
-    
-        
 
+def get_ports(ports):
+    ports = ports.split(',')
+    port_list = []
+    for item in ports:
+        if '-' in item:
+            start, stop = list(map(int, item.split('-')))
+            for i in range(start, stop + 1):
+                port_list.append(i)
+        else:
+            port_list.append(int(item))
+    return port_list
+
+
+if __name__ == '__main__':
+    ip = input('Enter hostname/ip: ')
+    if not ip:
+        print('Please enter ip or hostname!')
+    ports = input('Enter port range (ex. 80, 137-139): ')
+    if not ports:
+        print('Please enter port range!')
+    timeout = input('Enter timeout: ')
+    if not timeout:
+        timeout = 5
+    else:
+        timeout = int(timeout)   
+    try:
+        ip = socket.gethostbyname(ip)
+    except Exception as e:
+        print("Can't resolve ip: %s" %(e))
+        sys.exit(1)
+    port_list = get_ports(ports)
+    scan_ports(ip, port_list, timeout)
