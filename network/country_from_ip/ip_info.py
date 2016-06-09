1df5e571cf9ba82aca98209658c9dd95ae76d0ba
itjune <spirit1334@yandex.ru>
2016-06-09 23:14:33 +0500

    add flag

--- network/country_from_ip/ip_info.py
+++ network/country_from_ip/ip_info.py
@@ -2,11 +2,21 @@ from bs4 import BeautifulSoup
 from urllib import request
 
 ip = input('Enter ip: ')
+full = input('Do you need more info [no]: ')
 
 data = request.urlopen('https://db-ip.com/' + ip)
 soup = BeautifulSoup(data, "html.parser")
 
 
-for i in range(len(soup.find_all('th'))):
-	print(soup.find_all('th')[i].text, '\t',
-	      soup.find_all('td')[i].text.strip())
+if full:
+    for i in range(len(soup.find_all('th'))):
+        if soup.find_all('th')[i].text != 'Local time':
+            print(soup.find_all('th')[i].text, '\t',
+                  soup.find_all('td')[i].text.strip())
+else:
+    for i in range(len(soup.find_all('th'))):
+        if soup.find_all('th')[i].text == 'Country':
+            print(soup.find_all('th')[i].text, '\t',
+                  soup.find_all('td')[i].text.strip())
+            break
+    
